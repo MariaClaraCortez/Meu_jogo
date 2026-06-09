55453bb1743f87cfaea555b7aff3312a12e74cb4
Maria Clara Cortez <maria.c.dias@edu.senai.br>
2026-06-09 09:23:03 -0300

    add fotos

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+
+pygame.init()
+
+
+clock = pygame.time.Clock()
+
+#cria a janela do jogo
+tela = pygame.display.set_mode((1200,800))
+pygame.display.set_caption("Beber ou Morrer")
+
+
+
+
+
+
+
+
+
+
