ffeee867b2d583a66aae5f4aedccb51c9fa30ffb
Maria Clara Cortez <maria.c.dias@edu.senai.br>
2026-07-28 09:59:34 -0300

    terminado

--- main.py
+++ main.py
@@ -87,6 +87,8 @@ while rodando:
         tela.blit(texto_pontos,(520,40))
         texto_poderes = fonte_texto.render(f"PODERES: {uso}",False,(0,0,0))
         tela.blit(texto_poderes,(100,755))
+        texto_pausa = fonte_texto.render('Aperte Q para pausar',False,(0,0,0))
+        tela.blit(texto_pausa,(900,55))
 
 
         cont_sprite += 1
